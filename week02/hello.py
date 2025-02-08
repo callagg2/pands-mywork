823264de2b5b5113db801e040a184df0bd0ff5a3
gerry <gerrycallaghan2@gmail.com>
2025-02-08 21:34:16 +0000

    week 02

---
+++ week02/hello.py
@@ -0,0 +1,11 @@
+# hello.py
+# This program just requests you to input a variable and then puts it in a sentence
+# Author: Gerry Callaghan 
+
+
+print ("Hello Gerry")
+name = input("Enter your name:")
+print('Hello ' + name + '\nNice to meet you')
+print("or")
+print(f'Hello {name} \nNice to meet you')
+
