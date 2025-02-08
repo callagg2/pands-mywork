1a5a793b46516da95dba723bf5044d9756a329cd
callagg2 <gerrycallaghan2@gmail.com>
2025-02-08 21:49:36 +0000

    Add files via upload

---
+++ addOne.py
@@ -0,0 +1,7 @@
+# addOne.py
+# This program just performs a simple addition
+# Author: Gerry Callaghan 
+
+number=int(input('please enter a number: '))
+newNumber = number +1
+print (f'{number} plus one is {newNumber}')
---
+++ nameAndAge.py
@@ -0,0 +1,12 @@
+# nameAndAge.py
+# This programs prompts you to input a name and add and performs some adjustements on it
+# author: gerry callaghan
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print(f'Hello {name}, \t your age is {age}')
+print(21-4)
+
+print(2==3)
+
+      
