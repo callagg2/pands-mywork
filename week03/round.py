c793e6795ab330eedf76d00cb6b6a3466fd3f364
callagg2 <gerrycallaghan2@gmail.com>
2025-02-15 17:59:52 +0000

    another file we did today

---
+++ week03/round.py
@@ -0,0 +1,7 @@
+# rounds a number to the nearest whole number
+# author: Gerry Callaghan
+
+
+numberToRound = float(input("Enter a float number:"))
+roundedNumber = round(numberToRound)
+print ( f'{numberToRound} rounded is {roundedNumber}')
