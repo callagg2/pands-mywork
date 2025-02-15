0ad676d468aaf3b95c1644b889cbdbe35ec5bf80
callagg2 <gerrycallaghan2@gmail.com>
2025-02-15 17:49:43 +0000

    Delete floor.py

--- floor.py
+++
@@ -1,9 +0,0 @@
-# floors a number.
-#
-# Author: Gerry Callaghan
-
-import math
-numberTofloor = float(input("Enter a float number:"))
-flooredNumber = math.floor(numberTofloor)
-
-print(f'{numberTofloor} floored is {flooredNumber}')
